35c77b44f8577f5ae34a409945d813d3131bae92
김민호 <2022305011@skuniv.ac.kr>
2025-09-28 15:50:33 +0900

    🎉 Start: First Commit

---
+++ src/main/java/com/NeulBom/NeulBomApplication.java
@@ -0,0 +1,13 @@
+package com.NeulBom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeulBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeulBomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NeulBom/NeulBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NeulBom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeulBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
